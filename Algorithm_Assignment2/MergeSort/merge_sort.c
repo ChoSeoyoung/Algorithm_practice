57d4822bec67200204fe15a2d4caa4beadf89f5b
Choseoyoung <seoyoung0000@naver.com>
2022-04-08 23:11:28 +0900

    Merge Sort with C

---
+++ Algorithm_Assignment2/MergeSort/merge_sort.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 10000
+int sorted[MAX];//추가적인 공간 필요
+
+void merge(int list[], int low, int mid, int high) {
+    int l = low;
+    int h = mid + 1;
+    int idx = 0;
+    while (l < mid + 1 && h < high + 1) {
+        if (list[l] <= list[h]) {
+            sorted[idx] = list[l];
+            l += 1;
+            idx += 1;
+        }
+        else if (list[l] > list[h]) {
+            sorted[idx] = list[h];
+            h += 1;
+            idx += 1;
+        }
+    }
+    for (int i = l; i < mid + 1; i++) {
+        sorted[idx] = list[i];
+        idx += 1;
+    }
+    for (int i = h; i < high + 1; i++) {
+        sorted[idx] = list[i];
+        idx += 1;
+    }
+
+    int start = 0;
+    for (int i = low; i < high + 1; i++) {
+        list[i] = sorted[start++];
+    }
+}
+
+void merge_sort(int list[], int low, int high) {
+    int mid;
+
+    if (low < high) {
+        mid = (low + high) / 2; // 중간 위치를 계산하여 리스트를 균등 분할 -분할(Divide)
+        merge_sort(list, low, mid); // 앞쪽 부분 리스트 정렬 -정복(Conquer)
+        merge_sort(list, mid + 1, high); // 뒤쪽 부분 리스트 정렬 -정복(Conquer)
+        merge(list, low, mid, high); // 정렬된 2개의 부분 배열을 합병하는 과정 -결합(Combine)
+    }
+}
+
+void main() {
+    int list[MAX];
+    clock_t before;
+    double result;
+
+    srand(time(NULL));
+    for (int i = 0; i < MAX; i++) {
+        list[i] = rand() % MAX;
+    }
+
+
+    before = clock();
+    merge_sort(list, 0, MAX - 1);
+    result = (double)(clock() - before) / CLOCKS_PER_SEC;
+    printf("걸린시간은 %5.5f 입니다.", result);
+    return 0;
+}
