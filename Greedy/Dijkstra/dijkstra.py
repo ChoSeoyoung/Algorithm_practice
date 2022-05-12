06be36a2895f082a1a28168898131f2007cd37ec
Choseoyoung <seoyoung0000@naver.com>
2022-05-12 17:56:43 +0900

    Greedy/Dijkstra

--- Greedy/Dijkstra/dijkstra.py
+++ Greedy/Dijkstra/dijkstra.py
@@ -22,18 +22,15 @@ W3 = [[INF,7,1,5,INF],
      [INF,1,INF,INF,INF]]
 
 # result
-touch=[0,-1,-1,-1,-1]
 length=[0,0,0,0,0]
 result=[]
 
-# 가중치포함 방향그래프에서 ㅍ
 # printing for edge and weight
 def dijkstra(N, W):
     no_edge = 0
     vnear=0
 
     for i in range(1, N, 1):
-        touch[i]=0
         length[i]=W[0][i]
 
     while (no_edge < N - 1):
@@ -48,7 +45,6 @@ def dijkstra(N, W):
         for i in range(1, N, 1):
             if(length[vnear]+W[vnear][i]<length[i]):
                 length[i]=length[vnear]+W[vnear][i]
-                touch[i]=vnear
         length[vnear]=-1
         
         no_edge+=1
